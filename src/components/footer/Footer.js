338a616e412c66f0ef818f7f6ac2b407e1097230
Sterly Deracy <deracys1@tcnj.edu>
2023-05-22 18:58:35 -0400

    Change footer link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://linkedin.com/sterlyderacy'
+      href='https://linkedin.com/in/sterlyderacy'
       className='link footer_link'
     >
       Sterly Deracy © 2023
