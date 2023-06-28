ba868b8ab39a226cc66bd25b7b4a7b7a1dd6b09a
Sterly Deracy <deracys1@tcnj.edu>
2023-06-28 10:53:48 -0400

    Update skills to include new frameworks

--- src/portfolio.js
+++ src/portfolio.js
@@ -24,7 +24,19 @@ const projects = [
     name: 'The G.O.O.N. Card',
     description:
       'LinkTree and other applications have a static set of links that can be shared with other users, which is inconvenient if you want to share your LinkedIn with an employer but your instagram with a friend. The G.O.O.N., the most flexible networking application available, mitigates this issue by leeting the user select the handles they want to share on the fly!',
-    stack: ['HTML', 'CSS', 'JavaScript', 'NodeJS', 'ExpressJS', 'Bootstrap', 'SQLite', 'Google Charts API', 'Web Speech API', 'Git', 'Github'],
+    stack: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'NodeJS',
+      'ExpressJS',
+      'Bootstrap',
+      'SQLite',
+      'Google Charts API',
+      'Web Speech API',
+      'Git',
+      'Github',
+    ],
     sourceCode: 'https://www.github.com/sderacy/gooncard',
     livePreview: 'https://www.github.com/sderacy/gooncard',
   },
@@ -32,12 +44,20 @@ const projects = [
     name: 'HackTCNJ',
     description:
       'HALO is a web application that alleviates hackathon operations. From handling registration to hacker and organizer management, HALO works as a resource management tool for all organizers.',
-    stack: ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'Django', 'Git', 'Github'],
+    stack: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Bootstrap',
+      'Django',
+      'Git',
+      'Github',
+    ],
     sourceCode: 'https://www.github.com/tcnj-acm/halo',
     livePreview: 'https://www.hacktcnj.com',
   },
   {
-    name: 'Wally\'s Adventure',
+    name: "Wally's Adventure",
     description:
       'Hello! Welcome to Wallys Adventure! This is a simple arcade game that features Wally the Steam Whistle, the last steam whistle to be heard in Trenton. The idea for this game came from a class project that explored the relationship between Wally and the man who preserved him, William Lloyd Miller.',
     stack: ['HTML', 'CSS', 'JavaScript', 'Rails', 'PostgreSQL'],
@@ -47,7 +67,7 @@ const projects = [
   {
     name: 'AMSTR - ACCR Management System Tracker',
     description:
-      'ACCR is a nonprofit that works to provide additional support for defendants in the Atlantic region since the defendants in this area are overworked with too many cases. We created a system to record when the ACCR staff, lawyers, and other legal workers donate time, money, and efforts into the organization\'s efforts through a timesheet based system which also uses a database to store and collect the data. We plan to make this system as seamless as possible so that it is both efficient and effective, while maintaining all functionality.',
+      "ACCR is a nonprofit that works to provide additional support for defendants in the Atlantic region since the defendants in this area are overworked with too many cases. We created a system to record when the ACCR staff, lawyers, and other legal workers donate time, money, and efforts into the organization's efforts through a timesheet based system which also uses a database to store and collect the data. We plan to make this system as seamless as possible so that it is both efficient and effective, while maintaining all functionality.",
     stack: ['HTML', 'CSS', 'JavaScript', 'Rails', 'PostgreSQL'],
   },
 ]
@@ -57,10 +77,14 @@ const skills = [
   'AngularJS',
   'Bootstrap',
   'C',
+  'Canva',
   'C++',
   'CSS',
+  'Django',
+  'ExpressJS',
+  'Figma',
   'Flask',
-  'Git',
+  'Git & Github',
   'HTML',
   'Ionic Framework',
   'Java',
@@ -68,7 +92,7 @@ const skills = [
   'Kotlin',
   'MariaDB',
   'MySQL',
-  'Node.js',
+  'NodeJS',
   'PHP',
   'PostgreSQL',
   'Python',
@@ -76,7 +100,8 @@ const skills = [
   'React',
   'Ruby',
   'TypeScript',
-  'WordPress'
+  'Wix',
+  'Wordpress',
 ]
 
 const contact = {
